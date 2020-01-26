5c8c8cacf34e26d5e6ad2e54066e4b5c67ce942c
Meddy <meddy.seize@gmail.com>
2020-01-26 11:56:50 +0100

    Ajout de l'affichage de toute les formations

--- catalog.php
+++ catalog.php
@@ -3,7 +3,10 @@
 <?php
     include 'element/header.php';
     require 'fonc/fonctions.php';
-    $Formation = getFormation($_GET['id']);
+    $allFrm = getAllFrm();
+    if(isset($_GET['id'])){ //Si une variable id existe (dans l'adresse) alors on appelle la fonction getCategFrm
+    $categFrm = getCategFrm($_GET['id']);
+    }
 ?> 
 <head>
   <!--FICHIERS CSS -->
@@ -23,23 +26,47 @@
   
 
   <div class="col-xl-12 my-auto">
+  <?php
+  if(isset($_GET['id'])== FALSE){
+  ?>
+    <div class="Htitle">
+        <h1 style="text-align: center">Toutes les formations</h1>
+    </div>
+    <hr>
+  <?php
+    foreach($allFrm as $aLine){
+  ?>
+  <div class="card">
+      <h5 class="card-header"><?php echo($aLine['form_nom']); ?></h5>
+      <div class="card-body">
+        <?php echo($aLine['form_desc']); ?>
+      </div>
+    </div>
+<?php
+   }
+  }
+  else{
+?>
+
+  <!--Cette partie du code correspond au catalogue filtré par catégorie -->
     <?php 
       getnomCateg()
     ?>
   </div>
  <hr>
   <?php
-    foreach($Formation as $line){ // affiche chaque formation qu'il y a dans la catégorie correspondante
+    foreach($categFrm as $cLine){ // affiche chaque formation qu'il y a dans la catégorie correspondante
   ?>
   <div class="col-xl-12 my-auto">
     <div class="card">
-      <h5 class="card-header"><?php echo($line['form_nom']); ?></h5>
+      <h5 class="card-header"><?php echo($cLine['form_nom']); ?></h5>
       <div class="card-body">
-        <?php echo($line['form_desc']); ?>
+        <?php echo($cLine['form_desc']); ?>
       </div>
     </div>
   </div>
   <?php
+    }
   }
 ?>
   </div>
