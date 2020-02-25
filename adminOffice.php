5c59a7ca48d9fd0e203c7ea5c6bcb210271dc276
Meddy <meddy.seize@gmail.com>
2020-02-25 13:46:49 +0100

    Modification du backoffice

---
+++ adminOffice.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <!--FICHIERS CSS -->
+  <link rel="stylesheet" href="css/style2.css">
+  <!-- META -->
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--POLICES-->
+  <link href="https://fonts.googleapis.com/css?family=Bungee+Inline" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <!-- FICHIERS JAVASCRIPT -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <!-- TITRE -->
+  <title>Maison de la Formation Continue</title>
+</head>
+
+<body>
+    
+</body>
+</html>
---
+++ element/connexionAdm.php
@@ -0,0 +1,12 @@
+<form action="fonc/fonctions.php" method="post">			
+  <div class="form-group">
+    <label for="mail">Email</label>
+    <input type="text" class="form-control" id="loginAdm" name="loginAdm" placeholder="Votre login">
+  </div>
+  <div class="form-group">
+    <label for="passwd">Mot de passe</label>
+    <input type="password" class="form-control" id="passwdAdm" name="passwdAdm" placeholder="Mot de passe">
+  </div>
+  <input type="submit" class="btn btn-primary btn-lg btn-block" name="envoiAdm" value="Se connecter">
+  </form>
+  <br><br>
