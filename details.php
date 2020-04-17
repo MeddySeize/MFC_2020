c5d9dabe466626d3b4971d669e5939ed2ee75e61
Meddy <meddy.seize@gmail.com>
2020-04-17 15:14:35 +0200

    Ajout des informations de la formation

--- details.php
+++ details.php
@@ -22,19 +22,24 @@
     <div class="col-xl-12 center">
       <div class="Htitle">
         <h1 style="text-align: center;"><?php echo($dLine['form_nom']); ?></h1>
-        <hr> <!--Mettre une ombre-->
+        <hr>
       </div>
     </div>
   <div class="row">
-  <div class="col-xl-2"></div>
+  <div class="col-xl-2"></div> <!--Ajuster/equilibrer la page-->
     <div class="col-xl-3">
       <div class="box">
-         
+         <!-- Pour mettre une image -->
       </div>
     </div>
     <div class="col-xl-6">
         <p style="font-size: 20px"><?php echo($dLine['form_desc']); ?></p>
+        <p style="font-size: 20px">Lieu: <b><?php echo($dLine['form_ville'])?></b></p>
+        <p style="font-size: 20px">Niveau: <b><?php echo($dLine['form_niveau'])?></b></p>
+        <p style="font-size: 40px">Prix: <b><?php echo($dLine['form_prix'])?>€</b></p>
     </div>
+    
+       <input type="submit"  class="btn btn-primary btn-lg btn-block" name="envoiFormInsc" value = "S'inscrire">
   </div>
   </div>
   <?php 
